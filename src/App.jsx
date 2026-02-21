672ee8456b4b47e350773a7fddc2b662720f3968
Piyush Kumar Shrivastwa <piyushksshrivastawa@gmail.com>
2026-02-21 13:06:12 +0530

    fixed import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Display from './components/Display.Jsx'
-import Production from './components/Production'
-import Banner from './components/Banner'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Display from '../components/Display.Jsx'
+import Production from '../components/Production'
+import Banner from '../components/Banner'
+import Main from '../components/Main'
+import Footer from '../components/Footer'
 
 
 
